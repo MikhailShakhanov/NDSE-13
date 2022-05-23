69ec847ea0b543be22dc1532b49293c224b205d8
mi.shakhanov <mi.shakhanov@jet.su>
2022-05-23 09:56:27 +0300

    commit

--- 002-console/task1/task1.js
+++ 002-console/task1/task1.js
@@ -21,10 +21,56 @@ function getDateParams() {
 
 }
 
+//последний день месяца
+function getMonthLastDay(date) {
+  return (new Date(date.getFullYear(), date.getMonth() + 1, 0)).getDate();
+}
+
+//високосный
+function isLeapYear(date) {
+  return date.getFullYear() % 4 === 0;
+}
+
+//вычисление даты
+function calculateDate(sign = 1, y = 0, m = 0, d = 0) {
+
+  const date = new Date();
+  const newDate = new Date();
+
+  newDate.setDate(1);
+  const monthsToCalc = y ? 12 * y + m : m;
+
+  newDate.setMonth(date.getMonth() + sign * monthsToCalc);
+
+  if (m || y) {
+    //если сегодня последний день месяца, то после прибавления/вычитания месяцев тоже должен быть последний день
+    if (date.getDate() === getMonthLastDay(date)) {
+      newDate.setDate(getMonthLastDay(newDate));
+    }
+    //если итоговый месяц февраль, а текущая дата 31,30,29, то берем последний день февраля новой даты
+    else if (newDate.getMonth() === 1 &&
+      (date.getDate() === 31 || date.getDate() === 30 || date.getDate() === 29)) {
+      newDate.setDate(getMonthLastDay(newDate));
+    }
+    //в остальных случаях оставляем тот же день месяца
+    else {
+      newDate.setDate(date.getDate());
+    }
+  }
+
+  if (d) {
+    newDate.setDate(newDate.getDate() + sign * d);
+  }
+
+  console.log('New Date:', newDate);
+}
+
 const nowDate = new Date();
 const dateParams = {};
 getDateParams();
 
+console.log(getMonthLastDay(new Date()));
+console.log(isLeapYear(new Date(2020, 1, 1)));
 
 //если передали current
 if (argv._.includes('current')) {
@@ -37,12 +83,14 @@ if (argv._.includes('current')) {
 }
 
 //если передали add или sub
-if (argv._.includes('current')) {
-  if (Object.keys(dateParams).length) {
+if (argv._.includes('add')) {
+  /*if (Object.keys(dateParams).length) {
     for (let val in dateParams)
       console.log(dateParams[val]);
   } else {
     console.log(nowDate.toISOString());
-  }
+  }*/
 }
+
+calculateDate(1, 1, 2, 5);
 
