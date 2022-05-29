1fb4a7b8b583a4589c97569fcbc760dad4a60bda
mi.shakhanov <mi.shakhanov@jet.su>
2022-05-29 17:19:53 +0300

    commit

--- 004-stream/task1/task1.js
+++ 004-stream/task1/task1.js
@@ -8,20 +8,22 @@ const argv = yargs(hideBin(process.argv)).argv;
 const readline = require('readline');
 const CoinGame = require('./CoinGame');
 
-const fileName = argv._[0];
-const writeStream = fs.createWriteStream(fileName, { flags: 'a' });
+if (argv.GameFileName) {
+  const fileName = argv.GameFileName;
+  const writeStream = fs.createWriteStream(fileName, { flags: 'a' });
+  const input = readline.createInterface(process.stdin);
+  const guessValue = new CoinGame();
 
-const input = readline.createInterface(process.stdin);
-
-const guessValue = new CoinGame();
-
-input.on('line', (data) => {
-  writeStream.write(
-    `Игра: ${guessValue.getGameName()};\n` +
-      `Результат: ${guessValue.tryToGuess(data)}\n`,
-    'UTF8',
-  );
-  writeStream.end();
-  input.close();
-});
-input.on('close', () => console.log('This is the end of the game'));
+  input.on('line', (data) => {
+    writeStream.write(
+      `Игра: ${guessValue.getGameName()};` +
+        `Результат: ${guessValue.tryToGuess(data)}\n`,
+      'UTF8',
+    );
+    writeStream.end();
+    input.close();
+  });
+  input.on('close', () => console.log('This is the end of the game'));
+} else {
+  console.log('GameFileName argument required!');
+}
