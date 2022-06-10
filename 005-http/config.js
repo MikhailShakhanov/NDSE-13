b610fd48b7413b0a587011d2ef177ce6f6c16628
mi.shakhanov <mi.shakhanov@jet.su>
2022-06-10 18:30:31 +0300

    remove other br

--- 005-http/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  WEATHERSTACK_TOKEN:
-    process.env.WEATHERSTACK_TOKEN || '6ad53859517d0e096ef0a27358fa6468',
-  WEATHERSTACK_URL: 'http://api.weatherstack.com/',
-  CITY: 'Moscow',
-};
-
-module.exports = config;
